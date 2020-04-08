e32e103737740284edb380aa4f1a8e40a5c2404a
Konstantin Batyushev <batyushev@gmail.com>
2020-04-08 23:32:58 +1000

    fix app file

--- src/App.js
+++ src/App.js
@@ -4,12 +4,4 @@ import React from 'react';
 // Components
 import Maps from 'Pages/Map';
 
-export const App = () => {
-
-  return (
-    <div>
-
-      <Maps />
-    </div>
-  );
-};
+export const App = () => <Maps />;
